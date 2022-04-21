4c16774b5ae49b206fc79fcf45cd0c076cc59d07
Jake Jaworski <jakejaworski28@gmail.com>
2022-04-20 17:08:01 -0700

    Create band_name_generator.py
    Python program that suggests band names to the user.

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print('Welcome to my Band Name Generator Program!')
+
+city = input('What city did you grow up in?\\n')
+
+pet_name = input('What is the name of your pet?\\n')
+
+band_name = city + ' ' + pet_name
+
+print('Your band name should be ' + band_name + '!')
